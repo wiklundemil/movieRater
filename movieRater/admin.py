cb7b143fb8b96bed7f02e89eff43b93d599b1566
wiklundemil <wiklund.emil2000@hotmail.com>
2023-11-15 14:46:05 +0100

    Fixed that our models now create tables in sqllite

---
+++ movieRater/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Role)
+admin.site.register(Post)
+admin.site.register(Rating)
--- movieRater/models.py
+++ movieRater/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+
 class Post(models.Model):
     """Model representing a post."""
-    postId       = models.IntegerField(max_length=11, unique=True)
+    postId       = models.IntegerField(unique=True)
     postMetadata = models.CharField(max_length=300, help_text="Contain the data for the post.")
     postUserId   = models.ForeignKey('User', on_delete=models.RESTRICT, null=True)
+
     def __int__(self):
         return self.postId
 
 class Rating(models.Model):
-    ratingId      = models.IntegerField(max_length=11, unique=True)
-    ratingRating  = models.IntegerField(max_length=2, help_text="Value from 0-5")
+    ratingId      = models.IntegerField(unique=True)
+    ratingRating  = models.IntegerField(help_text="Value from 0-5")
     ratingComment = models.CharField(max_length=300)
     ratingPost    = models.ForeignKey('Post', on_delete=models.RESTRICT, null=True) #Foregin key used because a rating can have one Post but a Post may have many ratings.
     #ratingUserId, this can be accessed via Post because post have a FK user
@@ -18,7 +20,7 @@ class Rating(models.Model):
         return self.ratingId
 
 class User(models.Model):
-    userId       = models.IntegerField(max_length=11, unique=True)
+    userId       = models.IntegerField(unique=True)
     userName     = models.CharField(max_length=28)
     userSurname  = models.CharField(max_length=28)
     userForename = models.CharField(max_length=28)
@@ -33,9 +35,9 @@ class Role(models.Model):
     def __str__(self):
         return self.role
 
-class Following(models.Model):
-    userId    = models.ForeignKey('User', on_delete=models.RESTRICT, null=True)
-    following = models.ForeignKey('Movie', on_delete=models.RESTRICT, null=True)
+#class Following(models.Model):
+    #userId    = models.ForeignKey('User', on_delete=models.RESTRICT, null=True)
+    #following = models.ForeignKey('User', on_delete=models.RESTRICT, null=True)
 
 
 
